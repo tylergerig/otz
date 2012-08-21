f57168b1d2ee4e995af6d7436400a967b76cb15b
Peter Wooley <peterwooley@gmail.com>
2012-08-20 23:07:02 -0700

    Added bullets (but no hit detection yet; that's next). Also gave it *some* style, if I can call it that. It now kind of feels like a tank game.

--- public/js/otz.js
+++ public/js/otz.js
@@ -4,18 +4,26 @@ socket.on('world', function(data) {
   world = data;
   jwerty.key('a/arrow-left', function() {
     socket.emit("move", {dir: "left"})
+    event.preventDefault();
   });
   jwerty.key('d/arrow-right', function() {
     socket.emit("move", {dir: "right"})
+    event.preventDefault();
   });
   jwerty.key('w/arrow-up', function() {
     socket.emit("move", {dir: "up"})
+    event.preventDefault();
   });
   jwerty.key('s/arrow-down', function(e) {
     socket.emit("move", {dir: "down"})
     event.preventDefault();
   });
+  jwerty.key('f/space', function(e) {
+    socket.emit('shoot');
+    event.preventDefault();
+  });
   render();
+  renderPlayers(world.players);
   renderPlayer(world.player, true);
 });
 socket.on('moved', function(data) {
@@ -27,22 +35,72 @@ socket.on('moved', function(data) {
   renderPlayer(data.player, you);
 });
 
+socket.on('bullet-moved', function(data) {
+  console.log('bullet-moved');
+  renderBullet(data.bullet);
+});
+
+socket.on('bullet-destroyed', function(data) {
+  removeBullet(data.bullet);
+});
+
+var renderBullet = function(bullet) {
+  var move = true;
+  var $el = $("#"+bullet.id);
+  if(!$el.length) {
+    $el = $("<div>").attr('id', bullet.id).addClass('bullet').hide().appendTo('.world');
+    move = false;
+  }
+  var offset = $(".row:eq("+bullet.location.y+") .cell:eq("+bullet.location.x+")").position();
+  if(move) {
+    $el.animate(offset, 100, "linear");
+  } else {
+    $el.css(offset).show();
+  }
+};
+
+var removeBullet = function(bullet) {
+  $("#"+bullet.id).addClass('destroyed').fadeOut("fast", function() { $(this).remove(); });
+}
+
+socket.on('quit', function(data) {
+  console.log('quit');
+  removePlayer(data.player);
+});
+
+var removePlayer = function(player) {
+   $("#"+player.id).remove();
+};
+
+var renderPlayers = function(players) {
+  for(var i = 0; i < players.length; i++) {
+    renderPlayer(players[i]);
+  }
+};
+
 var renderPlayer = function(player, you) {
   var move = true;
   var $el = $("#"+player.id);
   if(!$el.length) {
     $el = $("<div>").attr('id', player.id).addClass('player').hide().appendTo('.world');
-    console.log(you);
     if(you) $el.addClass('you');
     move = false;
   }
-  var offset = $(".row:eq("+player.location.y+") .cell:eq("+player.location.x+")").offset();
+  var offset = $(".row:eq("+player.location.y+") .cell:eq("+player.location.x+")").position();
   if(move) {
     $el.animate(offset, 500/player.speed, "linear");
   } else {
     $el.css(offset).fadeIn();
   }
 
+  if(player.dir) {
+    var angle = "0deg";
+    if(player.dir === 'right') angle = '90deg';
+    else if(player.dir === 'down') angle = '180deg';
+    else if(player.dir === 'left') angle = '270deg';
+
+    $el.css('transform', 'rotate('+angle+')');
+  }
 };
 
 var render = function() {
@@ -53,8 +111,6 @@ var render = function() {
     $world.append($row);
     for(var j = 0; j < world.map[i].length; j++) {
       var $cell = $("<div>").addClass('cell');
-      //if(world.player.location.x === j && world.player.location.y === i)
-      //  $cell.append("X");
       $row.append($cell.addClass(world.map[i][j].material));
     }
   }
